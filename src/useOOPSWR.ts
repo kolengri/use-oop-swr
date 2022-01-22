5ed5a355babf8f22c81d17374e0f894d938acbd6
Grigoriy Kolenko <kolenko@me.com>
2022-01-22 23:47:44 +0100

    Update args names

--- src/useOOPSWR.ts
+++ src/useOOPSWR.ts
@@ -4,16 +4,16 @@ import { plainToInstance, ClassTransformOptions } from 'class-transformer';
 
 interface UseOOPSWR {
   <T extends InstanceType<any>, RawResult = any, RawError = any>(
-    classObj: { new (): T },
+    classInstance: { new (): T },
     swrResult: SWRResponse<RawResult, RawError>,
     options?: ClassTransformOptions
   ): [RawResult extends Array<any> ? T[] : T, SWRResponse<RawResult, RawError>];
 }
 
-export const useOOPSWR: UseOOPSWR = (classObj, swrResult, options) => {
+export const useOOPSWR: UseOOPSWR = (classInstance, swrResult, options) => {
   const obj = JSON.parse(JSON.stringify(swrResult.data || {}));
   const data = useMemo(
-    () => plainToInstance<any, any>(classObj, obj, options),
+    () => plainToInstance<any, any>(classInstance, obj, options),
     [obj]
   );
 
