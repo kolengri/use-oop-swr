1fc43d42f241a5dceb8e63c763260da94ecd49fe
Grigoriy Kolenko <kolenko@me.com>
2022-01-23 11:26:10 +0100

    Add type exports

--- src/useOOPSWR.ts
+++ src/useOOPSWR.ts
@@ -2,12 +2,18 @@ import { useMemo } from 'react';
 import { SWRResponse } from 'swr';
 import { plainToInstance, ClassTransformOptions } from 'class-transformer';
 
-interface UseOOPSWR {
+export type UseOOPSWRResult<
+  T extends InstanceType<any>,
+  RawResult = any,
+  RawError = any
+> = [RawResult extends Array<any> ? T[] : T, SWRResponse<RawResult, RawError>];
+
+export interface UseOOPSWR {
   <T extends InstanceType<any>, RawResult = any, RawError = any>(
     classInstance: { new (): T },
     swrResult: SWRResponse<RawResult, RawError>,
     options?: ClassTransformOptions
-  ): [RawResult extends Array<any> ? T[] : T, SWRResponse<RawResult, RawError>];
+  ): UseOOPSWRResult<T, RawResult, RawError>;
 }
 
 export const useOOPSWR: UseOOPSWR = (classInstance, swrResult, options) => {
@@ -22,3 +28,5 @@ export const useOOPSWR: UseOOPSWR = (classInstance, swrResult, options) => {
 
   return [data, swrResult];
 };
+
+export default useOOPSWR;
